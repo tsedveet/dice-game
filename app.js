f0efaeefd6f6b78e6c435a150aec0a6c7051831d
tsedveet <tsedvee.tt@gmail.com>
2021-06-10 15:16:05 +0800

    roll dice button-g ajilladag bolgoloo

--- app.js
+++ app.js
@@ -8,11 +8,21 @@ var scores = [0, 0];
 var roundScore = 0;
 
 // Shoonii ali talaaraa buusniig hadgalgh huvisagch heregtei, 1-6 gesen utgiig ene huvisagchid sanamsarguigeer uusgej ugnu.
-var dice = Math.floor(Math.random() * 6) + 1;
-document.querySelector('#score-0').textContent = 0;
-document.querySelector('#score-1').textContent = 0;
+var diceNumber = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector('#current-0').textContent = 0;
-document.querySelector('#current-1').textContent = 0;
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
 
-document.querySelector('.dice').style.display = 'none';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+var diceDom = document.querySelector('.dice');
+diceDom.style.display = 'none';
+
+
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    var diceNumber = Math.floor(Math.random() * 6)+1;
+
+    diceDom.style.display = 'block';
+    diceDom.src = 'dice-' + diceNumber + '.png';
+});
