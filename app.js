9670ba7bf6157f1665734d7ec1442a93ab354671
tsedveet <tsedvee.tt@gmail.com>
2021-06-10 18:16:40 +0800

    hold button-g ajilladag bolgoj hojigchiig zarladag bolloo

--- app.js
+++ app.js
@@ -15,8 +15,6 @@ document.getElementById('current-0').textContent = '0';
 document.getElementById('current-1').textContent = '0';
 var diceDom = document.querySelector('.dice'); 
 diceDom.style.display = 'none';
-
-
 // shoog shideh event listener
 document.querySelector('.btn-roll').addEventListener('click', function(){
     
@@ -36,17 +34,39 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
         document.getElementById('current-' + activePlayer).textContent = roundScore;
     }
     else{
-        // 1 buusan tul toglogchiin eeljiig ene hesegt solij ugnu 
-        roundScore = 0;
-        document.getElementById('current-' + activePlayer).textContent = 0;
-        activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-        diceDom.style.display = 'none';
-        // if(activePlayer === 0){
-        //     activePlayer = 1;
-        // }else{
-        //     activePlayer = 0;
-        // }
+        switchToNextPlayer();
     }
 });
+
+//HOLD tovchnii event listener
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    scores[activePlayer] = scores[activePlayer] + roundScore;
+    // if(activePlayer === 0){
+    //     scores[0] = scores[0] + roundScore;
+    // }
+    // else{
+    //     scores[1] = scores[1] + roundScore;
+    // }
+    document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+    if(scores[activePlayer] >= 20){
+        document.getElementById('name-' +activePlayer).textContent = 'WINNER!!!';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    }
+    else{
+        switchToNextPlayer();
+    }
+    
+   
+});
+
+function switchToNextPlayer(){
+    roundScore = 0;
+    document.getElementById('current-' + activePlayer).textContent = 0;
+
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    diceDom.style.display = 'none';
+}
